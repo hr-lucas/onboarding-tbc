d45a3c9322cc6842384d9ae4ee2693737656b18c
hr-lucas <lucashenriqueribeiro2009@gmail.com>
2023-10-10 23:49:13 -0300

    correcao talwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  mode: 'jit',
   content: [
     "./index.html",
     "./src/**/*.{vue,html,js}",
