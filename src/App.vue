07861690de9925e182c4d6ac9be20c4f2f207fc3
ygor <ygorpb2@gmail.com>
2023-10-11 15:24:35 -0300

    HomeView 1.2v

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Header from './views/components/header.vue';
 </script>
 
 <template>
-  <main class=" bg-black h-full bg-[url('src/assets/logo-background.sv')] bg-no-repeat bg-center -z-10">
+  <main class=" bg-black h-full bg-[url('src/assets/logo-background.svg')] bg-no-repeat bg-center -z-10">
     <Header />
     <RouterView class="h-screen"/>
   </main>
