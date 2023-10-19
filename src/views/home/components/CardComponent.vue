988d77a358bc6b69604668ff1f1335ce3d54bd40
ygor <ygorpb2@gmail.com>
2023-10-19 00:42:03 -0300

    Termos de condição

--- src/views/home/components/CardComponent.vue
+++ src/views/home/components/CardComponent.vue
@@ -16,7 +16,7 @@ const props = defineProps({
                     class=" bg-[url('https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80')]  bg-cover  w-full h-full ">
                 </div>
                 <div class="w-full flex items-center p-3  absolute bottom-0 left-0 gap-3 ">
-                    <img class="rounded-full border-2 border-borderColor" src="../../../assets/Ellipse.png" alt="">
+                    <img class="rounded-full border-2 border-borderColor" src="@/assets/Ellipse.png" alt="">
                     <h3 class="text-sm  text-white">
                         {{ cardData.title }}
                     </h3>
--- src/views/scoltDetails/components/seeMoreComponent.vue
+++ src/views/scoltDetails/components/seeMoreComponent.vue
@@ -5,7 +5,7 @@
     <div class="flex flex-col w-80 h-72 bg-boxColor rounded  text-white max-xl:hidden p-1">
         <h1 class=" text-center text-3xl">Veja também!</h1>
         <div class="flex justify-center h-full">
-            <img class="h-full" src="../../../assets/Ellipse.png" alt="">
+            <img class="h-full" src="@/assets/Ellipse.png" alt="">
         </div>
         <p class=" text-center text-xl p-2">Pietra Rut </p>
     </div>
