5d10b2a8bed58403cd148a5c5719f38695064a2c
ygor <ygorpb2@gmail.com>
2023-10-18 22:24:27 -0300

    Mobile

--- src/views/scoltDetails/components/GalleryComponent.vue
+++ src/views/scoltDetails/components/GalleryComponent.vue
@@ -15,9 +15,9 @@ let images = [
 <template>
     <div class=" border-1 rounded border-borderColor w-full ">
         <div class="flex flex-wrap w-full justify-center">
-            <div v-for="img in images" :key="img.id" class=" w-64 h-96 p-3 cursor-pointer">
+            <div v-for="img in images" :key="img.id" class="max-sm:w-full w-64 h-96 p-3 cursor-pointer">
 
-                <img class="h-full "
+                <img class="h-full w-full"
                     src="https://images.unsplash.com/photo-1523381210434-271e8be1f52b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80">
             </div>
         </div>
