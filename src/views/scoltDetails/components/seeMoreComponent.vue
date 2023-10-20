79afa6a65e16dd95d6d923271ebcd8679fa57b73
ygor <ygorpb2@gmail.com>
2023-10-19 23:39:29 -0300

    Carrossel

--- src/views/scoltDetails/components/seeMoreComponent.vue
+++ src/views/scoltDetails/components/seeMoreComponent.vue
@@ -5,7 +5,7 @@
     <div class="flex flex-col w-80 h-72 bg-boxColor rounded  text-white max-xl:hidden p-1">
         <h1 class=" text-center text-3xl">Veja também!</h1>
         <div class="flex justify-center h-full">
-            <img class="h-full" src="@/assets/Ellipse.png" alt="">
+            <img class="h-full" src="https://splove.com.br/storage/img/653e69d521a5d3bfc190c11eb79a9f7a.jpg" alt="">
         </div>
         <p class=" text-center text-xl p-2">Pietra Rut </p>
     </div>
