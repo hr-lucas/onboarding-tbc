373d2572662fdebabf0438a57e8aae5d471862ca
ygor <ygorpb2@gmail.com>
2023-10-20 00:41:56 -0300

    Carrossel

--- src/views/scoltDetails/components/seeMoreComponent.vue
+++ src/views/scoltDetails/components/seeMoreComponent.vue
@@ -2,11 +2,13 @@
 <script setup>
 </script>
 <template>
-    <div class="flex flex-col w-80 h-72 bg-boxColor rounded  text-white max-xl:hidden p-1">
-        <h1 class=" text-center text-3xl">Veja também!</h1>
-        <div class="flex justify-center h-full">
-            <img class="h-full" src="https://splove.com.br/storage/img/653e69d521a5d3bfc190c11eb79a9f7a.jpg" alt="">
+    <div class="flex flex-col bg-boxColor rounded  text-white max-xl:hidden p-1">
+        <div class="h-full  w-72 ">
+            <h1 class=" text-center text-3xl">Veja também!</h1>
+            <div class="flex justify-center h-64">
+                <img class="h-full" src="https://splove.com.br/storage/img/653e69d521a5d3bfc190c11eb79a9f7a.jpg" alt="">
+            </div>
+            <p class=" text-center text-xl p-2">Pietra Rut </p>
         </div>
-        <p class=" text-center text-xl p-2">Pietra Rut </p>
     </div>
 </template>
