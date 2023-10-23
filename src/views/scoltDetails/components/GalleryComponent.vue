bcb655f3d8784cdf63534f8f8a6bb848123cbd8c
ygor <ygorpb2@gmail.com>
2023-10-23 00:34:49 -0300

    Requisições e dados

--- src/views/scoltDetails/components/GalleryComponent.vue
+++ src/views/scoltDetails/components/GalleryComponent.vue
@@ -17,10 +17,9 @@ setTimeout(() => {
 <template>
     <div class="flex">
         <div class="grid grid-cols-3 border-1 rounded border-borderColor w-full" id="container">
-            <div class="  overflow-hidden  p-1 cursor-pointer " v-for="(img, index) in props.data" :key="index">
-                <div class=" w-full h-full " v-if="img.tipo == 'image'" v-viewer="{ movable: false }">
-                    <img class="w-full h-full object-cover" :src="img.src" />
-                </div>
+            <div v-for="(img, index) in props.data" :key="index" class="w-full h-56 overflow-hidden  "
+                v-viewer="{ movable: false }">
+                <img v-if="img.tipo == 'image'" class="w-full h-full p-1 object-cover" :src="img.src" />
             </div>
         </div>
     </div>
